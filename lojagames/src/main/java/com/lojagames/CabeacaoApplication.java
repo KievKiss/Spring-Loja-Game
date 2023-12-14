22e29bf85624771534a84db68ad613286bf4859f
Kiev <kevinkiss2010@gmail.com>
2023-12-14 11:28:22 -0300

    Adicionando Spring com o banco de dados

---
+++ lojagames/src/main/java/com/lojagames/CabeacaoApplication.java
@@ -0,0 +1,13 @@
+package com.lojagames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabeacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabeacaoApplication.class, args);
+	}
+
+}
